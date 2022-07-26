a5f20cd893a4063be6a4a4789f2d1e8ba8d2fd26
Sena <senabahadiroglu0@gmail.com>
2022-07-27 00:21:06 +0300

    NavbarBaşlangıç

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+
+export default function Navbar() {
+  return (
+    <Segment inverted>
+    <Menu inverted pointing secondary>
+      <Menu.Item
+        name='home'
+        // active={activeItem === 'home'}
+        // onClick={this.handleItemClick}
+      />
+      <Menu.Item
+        name='messages'
+        // active={activeItem === 'messages'}
+        // onClick={this.handleItemClick}
+      />
+      <Menu.Item
+        name='friends'
+        // active={activeItem === 'friends'}
+        // onClick={this.handleItemClick}
+      />
+    </Menu>
+  </Segment>
+  )
+}
